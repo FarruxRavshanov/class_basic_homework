de57dd8048cf93616ea2d41ae8d288517e6480e0
FarruxRavshanov <farruxlynx@gmail.com>
2022-11-28 15:37:48 +0300

    attribute

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,2 +1,9 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+p1 = Person('Jhon')
+
+print(p1.name)
